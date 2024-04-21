56efe716bf4f2eb770fee1721f306496692763f8
Nataliya Povarova <59220002+NataliyaPovarova@users.noreply.github.com>
2024-04-21 14:07:10 +0300

    Stage 1
    A template provided in the task description

---
+++ ringbuf.hpp
@@ -0,0 +1,24 @@
+#ifndef RINGBUF_HPP
+#define RINGBUF_HPP
+
+
+template<typename T>
+class RingBuffer {
+
+    T* _buf;
+    size_t _size;
+    T* _pTail;
+    T* _pHead;
+    bool _empty;
+    RingBuffer(size_t sz);
+    _buf = new T[_size];
+    ~RingBuffer()
+    {
+        delete [] _buf;
+    }
+    
+
+};
+
+
+#endif // RINGBUF_HPP
